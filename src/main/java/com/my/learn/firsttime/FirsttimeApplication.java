2a8b3e794a11fa8785332b7be48ee580005e361f
nhocthongmap123 <68158536+nhocthongmap123@users.noreply.github.com>
2023-09-20 23:08:39 +0700

    controller and service without database

--- src/main/java/com/my/learn/firsttime/FirsttimeApplication.java
+++ src/main/java/com/my/learn/firsttime/FirsttimeApplication.java
@@ -3,11 +3,15 @@ package com.my.learn.firsttime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class FirsttimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirsttimeApplication.class, args);
 	}
+
 
 }
