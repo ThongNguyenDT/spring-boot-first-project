60d9d9395d2ae34b906854d2343c43d07b2d9590
nhocthongmap123 <68158536+nhocthongmap123@users.noreply.github.com>
2023-09-20 22:11:18 +0700

    create

---
+++ src/main/java/com/my/learn/firsttime/FirsttimeApplication.java
@@ -0,0 +1,13 @@
+package com.my.learn.firsttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsttimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsttimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/learn/firsttime/FirsttimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.learn.firsttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsttimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
